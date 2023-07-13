1b020004c9d00509d981d5991b4a9a1b8babe9a4
youraj-08 <yourajsharma2003.com>
2023-07-14 01:17:23 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+var year = new Date().getFullYear();
+
+function Footer(){
+    return <footer>
+        <p>Copyright © {year}</p>
+    </footer>
+}
+
+export default Footer;
